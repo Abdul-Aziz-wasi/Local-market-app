dd92c9a00d9ef952b57149f1c542b6d2dbbe02e7
Abdul Aziz <abdulaziz758015@gmail.com>
2025-07-14 19:30:43 +0600

    added social login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,7 +4,19 @@ import { AuthContext } from '../../contexts/AuthContext';
 import { toast } from 'react-toastify';
 
 const Login = ({ isOpen, onClose }) => {
-  const { loginUser } = useContext(AuthContext);
+  const { loginUser,loginWithGoogle } = useContext(AuthContext);
+
+  const handleGoogleLogin =()=>{
+    loginWithGoogle()
+    .thne(result=>{
+      console.log(result)
+    })
+    .catch(error=>{
+      console.log(error)
+      
+    })
+
+  }
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -25,7 +37,7 @@ const Login = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
+    <div className="fixed inset-0 bg-teal-900 bg-opacity-50 z-50 flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0, scale: 0.85 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -76,7 +88,7 @@ const Login = ({ isOpen, onClose }) => {
           >
             Login
           </button>
-          <button className="btn bg-white w-full text-black border-[#e5e5e5]">
+          <button onClick={handleGoogleLogin} className="btn bg-white w-full text-black border-[#e5e5e5]">
   <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
   Login with Google
 </button>
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../firebase/firebase.init';
+
+
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
     const [loading,setLoading]=useState(true);
@@ -14,6 +18,11 @@ const AuthProvider = ({children}) => {
     const loginUser =(email,password)=>{
         setLoading(true)
         return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const loginWithGoogle =()=>{
+        setLoading(true);
+        return signInWithPopup(auth,googleProvider)
     }
 
     const logOutUser =()=>{
@@ -38,6 +47,7 @@ const AuthProvider = ({children}) => {
         user,
         createUser,
         loginUser,
+        loginWithGoogle,
         logOutUser
         
         
